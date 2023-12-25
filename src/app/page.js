2044cbf78f242d753ec5d39bb4843c7c271a8322
Danya Baron <dbaron@wpi.edu>
2023-12-25 18:46:52 +0000

    Dec 25, 2023, 1:46 PM

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
      
-    </main>
+    // </main>
+
+    <div>
+      <Head>
+        Danya Baron Portfolio
+      </Head>
+      <main></main>
+    </div>
   )
 }
