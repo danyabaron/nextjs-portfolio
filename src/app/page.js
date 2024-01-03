ce04587bd187909c52fdc9bad4f7924e49c2c65f
Danya Baron <dbaron@wpi.edu>
2024-01-03 22:47:51 +0000

    Jan 3, 2024, 5:47 PM

--- src/app/page.js
+++ src/app/page.js
@@ -19,25 +19,33 @@ export default function Home() {
 
 
       <main className="bg-gradient-to-t from-gradient-top to-gradient-bottom px-10">
-        <section className=" min-h-screen">
+        <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between w-full">
             
             <ul className="flex items-center w-full">
               <li>
                 {/* sparkle img */}
-                <IoSparklesSharp className="cursor-pointer text-main-brown text-2xl mr-10"/>
+                <IoSparklesSharp className="cursor-pointer text-main-brown text-2xl mr-10 transform hover:scale-110"/>
               </li>
               <li>
-                <a href="#" className="text-xl font-helvetica text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-lg px-4 py-2 transform hover:scale-105 transition-all duration-300">About</a>
+                <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
+                transition-all duration-500">About</a>
               </li>
               <li>
-              <a href="#" className="text-xl font-helvetica text-main-brown m-10">Projects</a>
+              <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
+                transition-all duration-500">Projects</a>
               </li>
               <li>
-              <a href="#" className="text-xl font-helvetica text-main-brown m-10">Contact</a>
+              <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
+                transition-all duration-500">Contact</a>
               </li>
             </ul>
           </nav>
+
+
+          <div id="hero">
+            
+          </div>
        
           
         </section>
