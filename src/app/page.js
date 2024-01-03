c0421251bebb1d24c378905fd2cbff6266b9de7a
Danya Baron <dbaron@wpi.edu>
2024-01-03 21:48:37 +0000

    Jan 3, 2024, 4:48 PM

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+
 import Image from 'next/image';
 // import syles from"
 
@@ -21,6 +22,7 @@ export default function Home() {
             <ul>
               <li>
                 {/* sparkle img */}
+                <img src="src/assets/sparkle.png"/>
               </li>
               <li>
                 About
