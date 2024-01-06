f6ec301c312062aa77a64b4616e485f996a705fc
Danya Baron <dbaron@wpi.edu>
2024-01-06 21:51:31 +0000

    Jan 6, 2024, 4:51 PM

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
                 transition-all duration-500">Projects</a>
               </li>
               <li>
-              <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 ml-6 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
+              <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
                 transition-all duration-500">Contact</a>
               </li>
             </ul>
