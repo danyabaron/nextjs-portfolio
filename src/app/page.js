067ef7e24e10ff904acec45b71fd370269f0d56c
Danya Baron <dbaron@wpi.edu>
2024-01-06 21:40:17 +0000

    Jan 6, 2024, 4:40 PM

--- src/app/page.js
+++ src/app/page.js
@@ -20,13 +20,14 @@ export default function Home() {
 
       <main className="bg-gradient-to-t from-gradient-top to-gradient-bottom px-10">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between w-screen">
+          <nav className="py-10 mb-12 flex justify-end w-screen">
+            <IoSparklesSharp className="cursor-pointer text-main-brown text-2xl ml-10 transform hover:scale-110"/>
             
-            <ul className="flex items-center w-full">
-              <li>
-                {/* sparkle img */}
+            <ul className="flex items-center">
+              {/* <li>
+                
                 <IoSparklesSharp className="cursor-pointer text-main-brown text-2xl mr-10 transform hover:scale-110"/>
-              </li>
+              </li> */}
               <li>
                 <a href="#" className="text-xl font-helvetica drop-shadow-md text-main-brown m-10 hover:bg-main-brown hover:text-text-pink rounded-full px-4 py-2 transform hover:scale-105
                 transition-all duration-500">About</a>
