7934ad6113ea3b2f94d3c0b07b563c6f1cae2cf8
Danya Baron <dbaron@wpi.edu>
2024-01-07 21:31:55 +0000

    Jan 7, 2024, 4:31 PM

--- src/app/page.js
+++ src/app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
          </div>
             
             
-            <ul className="flex items-center">
+            <ul className="flex items-center animate-fade-right animate-ease-in">
               {/* <li>
                 
                 <IoSparklesSharp className="cursor-pointer text-main-brown text-2xl mr-10 transform hover:scale-110"/>
