fcc8472b6b2946a09dc9109cc42b120f32fbdfba
Danya Baron <dbaron@wpi.edu>
2024-02-03 23:14:19 +0000

    Feb 3, 2024, 6:14 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -27,11 +27,28 @@ export default function ProjMang() {
         </nav>
 
         <div id="projheader" class="w-screen rounded-3xl">
-            <div id="proj-text" className="columns-4 flex items-center bg-white flex-col justify-center w-screen py-8 mb-9 gap-x-1">
-                <h3 className="text-3xl font-helvetica-medium drop-shadow-sm mb-9 text-main-brown">Project Management Design</h3>
+            <div id="proj-text" className="columns-4 flex items-center bg-white flex-col justify-center w-screen py-8 gap-5">
+                <h2 className="text-3xl font-helvetica-medium drop-shadow-sm text-main-brown">Project Management Design</h2>
                 <p className="font-helvetica text-md text-main-brown px-5 text-center text-wrap max-w-[500px]">
-                Proj management software for design and code
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mauris nunc, ornare ut velit eu, 
+                    sagittis laoreet ante. Sed porttitor felis non ligula aliquam lacinia
                 </p>
+                <h3 className="text-xl font-helvetica-medium drop-shadow-sm text-main-brown">
+                    Timeline
+
+                </h3>
+                <p className="font-helvetica text-md text-main-brown px-5 text-center text-wrap max-w-[500px]">
+                    November-December (4 weeks)
+                </p>
+                <h3 className="text-xl font-helvetica-medium drop-shadow-sm text-main-brown">
+                    Design Role
+
+                </h3>
+
+                <p className="font-helvetica text-md text-main-brown px-5 text-center text-wrap max-w-[500px]">
+                    Product, UI/UX, Research
+                </p>
+
             </div>
             
 
