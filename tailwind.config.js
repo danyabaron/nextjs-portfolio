65c45c6b41ca5959e49e633f3210d7a8e1154ae6
Danya Baron <dbaron@wpi.edu>
2024-02-03 22:32:24 +0000

    Feb 3, 2024, 5:32 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}", // Add a comma here
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./projmang/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: [
     '!duration-[0ms]',
