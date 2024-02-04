606000e8de6fff2d593860244ef55325b57a34f0
Danya Baron <dbaron@wpi.edu>
2024-02-04 20:54:43 +0000

    Feb 4, 2024, 3:54 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -30,7 +30,7 @@ export default function ProjMang() {
         </nav>
 
         <div id="projheader" class="w-screen items-center flex flex-row gap-9 rounded-3xl">
-            <div id="proj-text" className="columns-4 flex  flex-col ml-12  w-full py-6 gap-5">
+            <div id="proj-text" className="columns-4 flex flex-col ml-12 justify-center w-64 gap-5">
                 <h2 className="text-3xl font-helvetica-medium drop-shadow-sm text-main-brown">Project Management Design</h2>
                 <p className="font-helvetica text-md text-main-brown px-5 text-center text-wrap max-w-[500px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mauris nunc, ornare ut velit eu, 
