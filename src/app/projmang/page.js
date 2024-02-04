27522ded1b4a93cea60a99ab967fd2bdef23cf15
Danya Baron <dbaron@wpi.edu>
2024-02-04 20:51:55 +0000

    Feb 4, 2024, 3:51 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -29,8 +29,8 @@ export default function ProjMang() {
         
         </nav>
 
-        <div id="projheader" class="w-screen items-center flex flex-row gap-y-3 rounded-3xl gap-9">
-            <div id="proj-text" className="columns-4 flex items-center flex-col justify-center w-full py-8 gap-5">
+        <div id="projheader" class="w-screen items-center flex flex-row gap-9 rounded-3xl">
+            <div id="proj-text" className="columns-4 flex items-center flex-col ml-12 justify-center w-full py-6 gap-5">
                 <h2 className="text-3xl font-helvetica-medium drop-shadow-sm text-main-brown">Project Management Design</h2>
                 <p className="font-helvetica text-md text-main-brown px-5 text-center text-wrap max-w-[500px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mauris nunc, ornare ut velit eu, 
@@ -55,7 +55,7 @@ export default function ProjMang() {
             </div>
             {/* width={300} height={300} */}
 
-            <div id="proj-img" className="columns-2 flex flex-col bg-white mr-6 justify-center gap-5">
+            <div id="proj-img" className="columns-2 flex flex-col bg-white mr-12 justify-center gap-5">
                 <Image src={ProjTile} alt="proj mang 1" width={1200} height={674} className="drop-shadow-md" />
                 <Image src={ProjTileDiag} alt="proj mang 2" width={1200} height={674} className="drop-shadow-md" />
 
