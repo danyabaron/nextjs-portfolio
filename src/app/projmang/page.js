ca7dcf26a239d3032ac01315a39caae85697a134
Danya Baron <dbaron@wpi.edu>
2024-02-09 00:07:17 +0000

    Feb 8, 2024, 7:07 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -219,7 +219,7 @@ export default function ProjMang() {
             </div> */}
 
         </div>
-        <div id="proto-grid" className="grid justify-center grid-row-3 grid-flow-col py-7 gap-6">
+        <div id="proto-grid" className="grid justify-center grid-cols-2 py-7 gap-6">
                 <div> 
                     <Image src={LoginProto} width={500} height={500} alt="login-proto" className="drop-shadow-md" />
                     <p>
