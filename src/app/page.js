32bdbe4988d24109b8b4831800e331b4691b393b
Danya Baron <dbaron@wpi.edu>
2024-02-10 23:36:31 +0000

    Feb 10, 2024, 6:36 PM

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,7 @@ import DigBalance from '../../public/assets/digbalance.png';
 import Solar from '../../public/assets/solarsystem.png';
 import Factor from '../../public/assets/7factorTile.png';
 import sparkle from '../../public/assets/sparkle.png';
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaBehance } from "react-icons/fa";
+
 // import ProjMang from './projmang/page.js'; 
 
 
