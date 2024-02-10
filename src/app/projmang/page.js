88a759a70bc7542a180d476630b9acc3f1472236
Danya Baron <dbaron@wpi.edu>
2024-02-10 23:21:39 +0000

    Feb 10, 2024, 6:21 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -110,9 +110,9 @@ export default function ProjMang() {
 
             <div id="intro-text">
                     <p className="font-helvetica text-md text-main-brown px-5 text-left flex-1 text-wrap max-w-[500px]">
-                        The Design and Code Club wants a page on their site that club members and officers can log into to join projects and teams within the club. Members will have access to their own project 
-                        Kanban-style board where they can track their tasks, prioritize, set deadlines, and effectively collaborate with other team members. The idea of this project was to have a Trello/Jira style software for the Club
-                        where Club Officers would have administrative privileges to create project boards that club members can join.
+                        The UCF Design and Code Club wants a page on their site that club members and officers can log into to join projects and teams within the club. Members will have access to their own project 
+                        Kanban-style board where they can track their tasks, prioritize, set deadlines, and effectively collaborate with other team members on the project. The idea of this project is to have a Trello/Jira style software for the 
+                        Design and Code Club where Club Officers would have administrative privileges to create project boards that club members can join.
 
                     </p>
                 </div>
