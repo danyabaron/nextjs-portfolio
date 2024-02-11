e6ddb22b340d84106a3087ab8cfed959245f210c
Danya Baron <dbaron@wpi.edu>
2024-02-11 02:02:10 +0000

    Feb 10, 2024, 9:02 PM

--- src/app/page.js
+++ src/app/page.js
@@ -182,7 +182,7 @@ export default function Home() {
             <h1 className='text-3xl font-helvetica-medium drop-shadow-md mb-9 text-main-brown'>Projects</h1>
 
             <div id="projects-0.5" className="grid grid-rows-1 justify-center items-center grid-flow-col">
-              <Image src={DCProj} alt="Design & Code Project Management" loading="lazy" className="rounded-2xl drop-drop-shadow-md"/>
+              <Image src={DCProj} alt="Design & Code Project Management" width={425} height={337} loading="lazy" className="rounded-2xl drop-drop-shadow-md"/>
             <div id="pented-info" className="bg-gradient-top flex flex-col drop-shadow-lg justify-center items-center m-12 p-4 gap-7 rounded-3xl">
                 <h2 className="font-helvetica-italic text-xl mt-5 text-main-brown">
                   UCF Design & Code Project Management
