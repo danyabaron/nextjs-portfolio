634202d72a0c61437e455a061d1279eacb20680e
Danya Baron <dbaron@wpi.edu>
2024-02-11 01:24:44 +0000

    Feb 10, 2024, 8:24 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -297,13 +297,13 @@ export default function ProjMang() {
                 </div> */}
                 <div id="features" className="flex flex-col items-center py-6 bg-white rounded-3xl drop-shadow-lg w-96">
                         <h2 className='text-2xl font-helvetica-italic drop-shadow-md text-center mb-5 text-hover-resume'>Features</h2>
-                        <ul className="list-disc list-inside font-helvetica text-lg text-main-brown bg-white text-wrap max-w-[300px]">
+                        <ul className="list-disc list-inside columns-2 font-helvetica text-lg text-main-brown bg-white text-wrap max-w-[300px]">
                             <li className="mb-3"> Project Kanban Board</li>
                             <li className="mb-3"> Edit task Kanban card </li>
                             <li className="mb-3"> Create task Kanban card </li>
                             <li className="mb-3"> Club officer admin view </li>
-                        </ul>
-                        <ul className="list-disc list-inside font-helvetica text-lg text-main-brown bg-white text-wrap max-w-[300px]">
+                        
+                       
                             <li className="mb-3"> Login Screen</li>
                             <li className="mb-3"> Main projects screen to see ongoing and past projects</li>
                             <li className="mb-3"> User Dashboard</li>
