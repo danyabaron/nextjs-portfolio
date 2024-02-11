b4a56fcafcf869d2041260721486dde8caaa88c7
Danya Baron <dbaron@wpi.edu>
2024-02-11 20:01:14 +0000

    Feb 11, 2024, 3:01 PM

--- src/app/projmang/page.js
+++ src/app/projmang/page.js
@@ -44,19 +44,6 @@ import { useRouter } from 'next/router';
 
 export default function ProjMang() {
 
-    const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      window.scrollTo(0, 0);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
 
 
 
