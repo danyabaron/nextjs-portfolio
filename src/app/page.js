3811f106eadc367949ba14c989980895915937b9
Danya Baron <dbaron@wpi.edu>
2024-02-15 01:47:13 +0000

    Feb 14, 2024, 8:47 PM

--- src/app/page.js
+++ src/app/page.js
@@ -63,12 +63,11 @@ export default function Home() {
 
 
           <nav className="py-10 mb-12 flex justify-between w-full">
-            <div className="flex items-center ml-10">
-    {/* IoSparklesSharp on the left */}
+            {/* <div className="flex items-center ml-10">
+
             <IoSparklesSharp className=" text-main-brown text-2xl transform hover:text-hover-resume transition-all duration-500 hover:scale-110" />   
-    {/* Other content on the left (if any) */}
-    {/* Add your additional content here */}
-         </div>
+    
+         </div> */}
             
             
             <ul className="flex items-center animate-fade-right animate-ease-in">
