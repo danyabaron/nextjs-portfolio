96d4a86df53d497eb018aec5ad0b5b995767948e
Danya Baron <dbaron@wpi.edu>
2024-02-17 22:30:54 +0000

    Feb 17, 2024, 5:30 PM

--- src/app/page.js
+++ src/app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
         
 
 
-          <nav className="py-10 mb-12 flex justify-center w-full">
+          <nav className="py-10 mb-12 flex justify-between w-full">
             <div className="flex items-center ml-10">
     {/* IoSparklesSharp on the left */}
     {/* transform hover:text-hover-resume transition-all duration-500 hover:scale-110" */}
